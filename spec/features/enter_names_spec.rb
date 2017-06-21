86aab15e9631d972a4fc8990ef8d219fcbe1b212
Corina <fluturelcorina@gmail.com>
2017-06-21 15:24:29 +0100

    Adding web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require './app.rb'
 
 feature 'Name form' do
   scenario 'expects players to fill in their names (in a form), submit that form, and see those names on-screen' do
-    visit '/'
-    fill_in('name1', with:'Tom')
-    fill_in('name2', with:'Jerry')
-    click_button('Submit names')
+    sign_in_and_play
     visit('/play')
     expect(page).to have_content('Tom')
     expect(page).to have_content('Jerry')
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,6 @@
+def sign_in_and_play
+  visit '/'
+  fill_in('name1', with:'Tom')
+  fill_in('name2', with:'Jerry')
+  click_button('Submit names')
+end
